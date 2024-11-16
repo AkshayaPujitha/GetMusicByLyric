a873272bbed1bf32110369bebe60d1a6f3939f86
pujitha <akshayapujithakolli@gmail.com>
2024-11-16 11:10:58 +0530

    Chore:locally tested

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -37,5 +38,5 @@ def show_lyrics(music_id):
         lyrics=music_data['lyrics']
     )
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
